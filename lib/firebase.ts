65c0e07f10f6d539350fd34c90c133446d1d6ca9
Benjamin1219-glitch <saitejak1219@gmail.com>
2026-01-05 21:03:42 +0530

    refactor: use NEXT_PUBLIC env vars for firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAavXPmCt9ZeI5re0-aKCDvN5D0v9nXYeg",
-    authDomain: "napkin-cdd41.firebaseapp.com",
-    projectId: "napkin-cdd41",
-    storageBucket: "napkin-cdd41.firebasestorage.app",
-    messagingSenderId: "820925836724",
-    appId: "1:820925836724:web:7a0a63689788da7f7296e1",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     measurementId: "G-W2R25RQ61E"
 };
 
